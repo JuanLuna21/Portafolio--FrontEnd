799fddbd56dd4de4db040d7850699c3a7b91dd6a
Juan Luna <xjuanigancio@gmail.com>
2022-08-19 17:39:40 -0300

    Fix Login.

--- Portafolio/FrontEnd/src/app/components/logo/logo.component.ts
+++ Portafolio/FrontEnd/src/app/components/logo/logo.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.css']
 })
+
 export class LogoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.router.navigate(['/login']);
+  }
 }
--- Portafolio/FrontEnd/src/app/service/person.service.ts
+++ Portafolio/FrontEnd/src/app/service/person.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { person } from '../model/person.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,10 @@ export class PersonService {
   url = 'http://localhost:8080/Persons';
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getPerson(): Observable<person> {
+    return this.http.get<person>(this.url + 'Person/bring');
+  }
+
 }
