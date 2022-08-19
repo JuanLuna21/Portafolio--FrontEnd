96ad884c867319c30764a777f645e41636bd0631
Juan Luna <xjuanigancio@gmail.com>
2022-08-19 19:36:55 -0300

    Making Login.

---
+++ Portafolio/BackEnd/src/main/java/com/portafolio/mgb/Security/Entity/PrincipalUser.java
@@ -0,0 +1,6 @@
+
+package com.portafolio.mgb.Security.Entity;
+
+public class PrincipalUser {
+    
+}
---
+++ Portafolio/BackEnd/src/main/java/com/portafolio/mgb/Security/Enums/RolName.java
@@ -0,0 +1,8 @@
+
+package com.portafolio.mgb.Security.Enums;
+
+public enum RolName {
+    
+    ROLE_ADMIN, ROL_USER
+   
+}
